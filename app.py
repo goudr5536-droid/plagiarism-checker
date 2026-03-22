aa2267b130ae3c6563951844d9cb11cf62c0d613
goudr5536-droid <goudr5536@gmail.com>
2026-03-22 22:44:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -125,5 +125,5 @@ def result():
     return render_template('result.html', submissions=submissions)
 
 if __name__ == '__main__':
-    port=int(os.environ.get("PORT",10000))
+    port=int(os.environ.get("PORT",5000))
     app.run(host='0.0.0.0, port=port')
